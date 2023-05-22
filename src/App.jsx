73594d2fc3dd21775061c14651c197aada4c1539
varunbr <varunbr6@gmail.com>
2023-05-22 15:44:06 +0530

    using modal dialog & children props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import PostList from "./components/PostList";
+import MainHeader from "./components/MainHeader";
+import { useState } from "react";
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function showModalHandler() {
+    setModalIsVisible(true);
+  }
+
   return (
-    <main>
-      <PostList />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostList
+          isPosting={modalIsVisible}
+          onClose={hideModalHandler}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
   );
 }
 
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
+import Modal from "./Modal";
 
-function PostList() {
+function PostList({ isPosting, onStopPosting }) {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
@@ -17,10 +18,15 @@ function PostList() {
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Ajay" body="This is body of Ajay" />
